62ac256a0d2d65bfb612f1ceb708954b25be242b
ujangsprr <ujang.supriyadi568@gmail.com>
2021-01-19 02:14:21 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from lexer import Lexer
+from parser import Parser
+from evaluator import Evaluator
+
+
+def main():
+    filename = 'test.code'
+    file = open(filename, 'r')
+    lexer = Lexer(file)
+    parser = Parser(lexer.tokens)
+
+    lexer.tokenizer()
+    print("TOKENS:")
+    print(lexer.tokens, "\n")
+
+    parser.build_AST()
+    print("AST:")
+    print(parser.AST, "\n")
+
+    evaluator = Evaluator(parser.AST)
+
+    print("OUTPUT:")
+    evaluator.run(parser.AST)
+
+
+if __name__ == "__main__":
+    main()
